1334f8d8227df860ea9df34b47fb755c111e535a
Deepanshu Ahuja <deepanshu.ahuja@tatapower-ddl.com>
2020-09-27 16:43:51 +0530

    Qr code

--- src/Routes.js
+++ src/Routes.js
@@ -11,8 +11,8 @@ export default class Routes extends Component {
         return (
 
             <Switch>
-                 {/* <Route exact path="/" component={Index}></Route> */}
-                 <Route exact path="/getstarted/:rid/:tableid" component={Index}></Route>
+                 <Route exact path="/" component={Index}></Route>
+                 <Route  path="/getstarted" component={Index}></Route>
                  <Route exact path="/login" component={Login}></Route>
                  <Route exact path="/qrcode" component={Qrcode}></Route>
                  <Route exact path="/restuarant" component={Restuarant}></Route> 
